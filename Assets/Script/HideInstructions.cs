e17392640cd4017523765d167919b4be3280f47d
Bazztoner <bazztoner.phoenix@gmail.com>
2018-09-02 14:28:26 -0300

    Instructions update

---
+++ Assets/Script/HideInstructions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HideInstructions : MonoBehaviour
+{
+    public GameObject inst;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            inst.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
