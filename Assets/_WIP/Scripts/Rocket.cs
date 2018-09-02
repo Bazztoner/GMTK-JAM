3fac6ed518a0f908f2d6cf180fd32eac77e6ccfd
Antumarin <antumarin@hotmail.com>
2018-09-01 22:15:21 -0300

    Added basic Rocket Jump mechanic (tested in different scene).

---
+++ Assets/_WIP/Scripts/Rocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+	public float speed, destroyTimer = 3;
+
+	private void Start()
+	{
+		Destroy(gameObject, destroyTimer);
+	}
+
+	void Update ()
+	{
+		transform.position += transform.right * speed * Time.deltaTime;
+	}
+
+	private void OnCollisionEnter(Collision c) { Destroy(gameObject); }
+	private void OnTriggerEnter(Collider c) { Destroy(gameObject); }
+	private void OnCollisionEnter2D(Collision2D c) { Destroy(gameObject); }
+	private void OnTriggerEnter2D(Collider2D c) { Destroy(gameObject); }
+}
